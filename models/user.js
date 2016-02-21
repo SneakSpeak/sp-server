d239b8a142beb9bc7a8c808eecdcf5d8aab1e83c
Ville Vaarala <ville.l.vaarala@gmail.com>
2016-02-21 21:45:03 +0200

    Create api/user/register route

--- models/user.js
+++ models/user.js
@@ -3,7 +3,6 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     username: DataTypes.STRING,
-    name: DataTypes.STRING,
     password: DataTypes.STRING,
     device: DataTypes.STRING
   }, {
--- routes/user.js
+++ routes/user.js
@@ -3,56 +3,55 @@
 var express = require('express');
 var router = express.Router();
 
+var models = require('../models');
+
 
 
 router.post('/login', function (req, res, next) {
-
+  
 });
 
 router.post('/register', function (req, res, next) {
-	console.log(req.body);/*
+	console.log(req.body);
 	var username = req.body.username;
-	var name = req.body.name;
 	var password = req.body.password;
 	if (!username) {
 		return res.status(400).json({error: 'InvalidUserName'});
-	}
-	if (!name) {
-		return res.status(400).json({error: 'InvalidName'});
 	}
 	if (!password) {
 		return res.status(400).json({error: 'InvalidPassword'});
 	}
 
-	  // TODO:
-	  // Hash passwords!!!!!
+  // TODO:
+  // Hash passwords!!!!!
 
-	  // Check if username exists -> Throw 409.
-	  var query = {where: {username: username}};
-	  models.User.findOne(query).then(function(user) {
-	  	if (user) {
-	  		return res.status(409).json({error: 'UserNameExists'});
-	  	}
-	  	models.User.create({
-	  		username: username,
-	  		name: name,
-	  		password: password //TODO: Save the hash, not the pass!!!
-	  	}).then(function userCreated(createdUser){
-	  		console.log(createdUser.get({
-	  			plain:true
-	  		});
-	  	}, function(err) {
-	  		return res.status(500).json({error: 'ServerError'});
-	  	});*/
-	res.sendStatus(200);
+  // Check if username exists -> Throw 409.
+  var query = {where: {username: username}};
+  models.User.findOne(query).then(function(user) {
+  	if (user) {
+  		return res.status(409).json({error: 'UserNameExists'});
+  	}
+  	models.User.create({
+  		username: username,
+  		password: password //TODO: Save the hash, not the pass!!!
+  	}).then(function userCreated(createdUser){
+  		console.log(createdUser.get({
+  			plain:true
+  		}));
+  		res.status(200).json(createdUser);
+  	}, function(err) {
+  		return res.status(500).json({error: 'ServerError'});
+  	});
+  //res.sendStatus(200);
+  });
 });
 
 router.get('/logout', function (req, res, next) {
-
+	res.sendStatus(200);
 });
 
 router.get('/:id/messages', function (req, res, next) {
-
+	res.sendStatus(200);
 });
 
 module.exports = router;
