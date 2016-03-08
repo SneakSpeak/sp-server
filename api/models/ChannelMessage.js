9d71aa343d65eecbc16b15d487126557386162a1
Ville Vaarala <ville.l.vaarala@gmail.com>
2016-03-08 21:43:58 +0200

    Add channel specific messages

---
+++ api/models/ChannelMessage.js
@@ -0,0 +1,29 @@
+/**
+ * ChannelMessage.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    // Sender
+    from: {
+      model: 'user',
+      required: true
+    },
+    // Receiver
+    channel: {
+      model: 'channel',
+      required: true
+    },
+    msg: {
+      type: 'text',
+      required: true
+    }
+
+
+  }
+};
+
