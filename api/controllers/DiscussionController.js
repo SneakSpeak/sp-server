7329fcf630cd98008460389a3560bde46bc5f3d7
Ville Vaarala <ville.l.vaarala@gmail.com>
2016-04-04 09:23:26 +0300

    Prevent listing private discussions

--- api/controllers/DiscussionController.js
+++ api/controllers/DiscussionController.js
@@ -1,3 +1,4 @@
+var actionUtil = require('../../node_modules/sails/lib/hooks/blueprints/actionUtil');
 /**
  * DiscussionController
  *
@@ -6,6 +7,30 @@
  */
 
 module.exports = {
-	
-};
+	find: function find(req, res ) {
 
+    var query = Discussion.find()
+      .where( actionUtil.parseCriteria(req) )
+      .where( {isChannel: true})
+      .limit( actionUtil.parseLimit(req) )
+      .skip( actionUtil.parseSkip(req) )
+      .sort( actionUtil.parseSort(req) );
+      query = actionUtil.populateRequest(query, req);
+      query.exec(function found(err, matchingRecords) {
+        if (err) return res.serverError(err);
+
+        // Only `.watch()` for new instances of the model if
+        // `autoWatch` is enabled.
+        if (req._sails.hooks.pubsub && req.isSocket) {
+          Discussion.subscribe(req, matchingRecords);
+          if (req.options.autoWatch) { Discussion.watch(req); }
+          // Also subscribe to instances of all associated models
+          _.each(matchingRecords, function (record) {
+            actionUtil.subscribeDeep(req, record);
+          });
+        }
+
+        res.ok(matchingRecords);
+      });
+    },
+};
